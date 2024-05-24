41d3e1bea23b39bfd98ff0ce4b786ba10836ba94
Kase1337 <linkinusf1@gmail.com>
2024-05-24 06:33:28 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) 
+  {
+    
+    //Print out the lyrics of the song
+    Console.WriteLine("Now, this is a story all about how");
+    Console.WriteLine ("My life got flipped-turned upside down");
+    Console.WriteLine ("And I'd like to take a minute");
+    Console.WriteLine ("And I'd like to take a minute");
+    Console.WriteLine ("I'll tell you how I became the prince of a town called Bel-Air");
+     
+    
+  }
+}
